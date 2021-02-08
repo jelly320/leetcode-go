1baee42d91def9c390c8bcdbc6d3986818515505
jelly320 <snsdzjlz@163.com>
2021-02-08 11:51:10 +0800

    v1 重新思考了下感觉理解了双指针了..

--- Array/0026.Remove Duplicates from Sorted Array.go
+++ Array/0026.Remove Duplicates from Sorted Array.go
@@ -2,24 +2,36 @@
 package main
 
 /*v0*/
+//func removeDuplicates(nums []int) int {
+//	m := map[int]int{}
+//	j := 0
+//	for i, v := range nums {
+//		if _, ok := m[v]; !ok {
+//			if len(m) != 0 {
+//				j++
+//				nums[i], nums[j] = nums[j], nums[i]
+//			}
+//			m[v] = i
+//		}
+//	}
+//	return len(m)
+//}
+
+/*v1*/
 func removeDuplicates(nums []int) int {
-	m := map[int]int{}
 	j := 0
-	for i, v := range nums {
-		if _, ok := m[v]; !ok {
-			if len(m) != 0 {
-				j++
-				nums[i], nums[j] = nums[j], nums[i]
-			}
-			m[v] = i
+	for i := range nums {
+		if nums[i] != nums[j] {
+			j++
+			nums[i], nums[j] = nums[j], nums[i]
 		}
 	}
-	return len(m)
+	return j + 1
 }
 
 func main() {
-	//nums := []int{0,0,1,1,1,2,2,3,3,4}
-	nums := []int{1, 1, 2}
+	nums := []int{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}
+	//nums := []int{1, 1, 2, 2}
 	aa := removeDuplicates(nums)
 	println(aa)
 
