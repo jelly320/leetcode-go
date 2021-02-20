31ae490371fceb93aab52dc2ff9f0577b6a118e1
jelly320 <snsdzjlz@163.com>
2021-02-20 11:56:41 +0800

    新增一种写法,go

--- note/二分查找.go
+++ note/二分查找.go
@@ -5,10 +5,11 @@ import (
 )
 
 /*寻找一个数（基本的二分搜索）*/
+//写法1:
 func binarySearch(nums []int, target int) int {
 	left := 0
 	right := len(nums) - 1
-	for {
+	for left < right {
 		mid := left + (right-left)/2
 		fmt.Printf("起始区间:[left,right]=[%d,%d],mid: %d\n", left, right, mid)
 
@@ -23,18 +24,36 @@ func binarySearch(nums []int, target int) int {
 			right = mid - 1
 			fmt.Printf("dayu;区间:[left,right]=[%d,%d]\n", left, right)
 		}
-		//} else if left >= right {
-		//	//循环退出条件
-		//	fmt.Printf("left: %d,right: %d",left,right)
-		//	break
-		//}
-		//设置循环的退出条件
-		if left > right {
-			break
-		}
 	}
 	return -1
 }
+
+//写法2:
+//func binarySearch(nums []int, target int) int {
+//	left := 0
+//	right := len(nums) - 1
+//	for left < right {
+//		mid := left + (right-left)/2
+//		fmt.Printf("起始区间:[left,right]=[%d,%d],mid: %d\n", left, right, mid)
+//
+//		if nums[mid] == target {
+//			return mid
+//		} else if nums[mid] < target {
+//			//如果小于target,则区间往右移
+//			left = mid + 1
+//			fmt.Printf("xiaoyu;区间:[left,right]=[%d,%d]\n", left, right)
+//		} else if nums[mid] > target {
+//			//如果大于target,则区间往左移
+//			right = mid - 1
+//			fmt.Printf("dayu;区间:[left,right]=[%d,%d]\n", left, right)
+//		}
+//		//设置循环的退出条件
+//		if left > right {
+//			break
+//		}
+//	}
+//	return -1
+//}
 
 /*寻找左侧边界的二分搜索*/
 func left_bound(nums []int, target int) int {
@@ -80,13 +99,13 @@ func main() {
 	//fmt.Printf("result: %d", index)
 
 	/*测试以下内容:*/
-	nums := []int{0, 0, 0, 1, 1, 1, 2, 2, 2, 3}
+	//nums := []int{0, 0, 0, 1, 1, 1, 2, 2, 2, 3}
 	//index := left_bound(nums,1)
 	//index := left_bound(nums,2)
-	index := left_bound(nums, 3)
+	//index := left_bound(nums, 3)
 	//index := left_bound(nums,-1)
 	//index := left_bound(nums,4)
 
-	fmt.Printf("result: %d", index)
+	//fmt.Printf("result: %d", index)
 
 }
