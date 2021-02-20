18aacdc13b1fe23d9454cf3ccd4566d3b5443f1a
jelly320 <snsdzjlz@163.com>
2021-02-20 14:24:17 +0800

    优化了,嘻嘻

--- Array/0035. Search Insert Position.go
+++ Array/0035. Search Insert Position.go
@@ -18,19 +18,52 @@ package main
 
 /*v1 二分查找?*/
 //题目给定的条件是一个"排好序的数组"
+//func searchInsert(nums []int, target int) int {
+//	left := 0
+//	right := len(nums) - 1
+//
+//	for {
+//		mid := left + (right-left)/2
+//
+//		if left > right {
+//			break
+//		}
+//		if nums[mid] == target {
+//			return mid
+//		} else if nums[mid] > target {
+//			right = mid - 1
+//		} else if nums[mid] < target {
+//			left = mid + 1
+//		}
+//	}
+//	return left
+//}
+
+/*参考 halfrost LeetCode-Go*/
+//这个是beat 100%的
+//func searchInsert(nums []int, target int) int {
+//	low, high := 0, len(nums)-1
+//	for low <= high {
+//		mid := low + (high-low)>>1
+//		if nums[mid] >= target {
+//			high = mid - 1
+//		} else {
+//			if (mid == len(nums)-1) || (nums[mid+1] >= target) {
+//				return mid + 1
+//			}
+//			low = mid + 1
+//		}
+//	}
+//	return 0
+//}
+/*参考后写的*/
 func searchInsert(nums []int, target int) int {
 	left := 0
 	right := len(nums) - 1
-
-	for {
+	for left <= right {
 		mid := left + (right-left)/2
 
-		if left > right {
-			break
-		}
-		if nums[mid] == target {
-			return mid
-		} else if nums[mid] > target {
+		if nums[mid] >= target {
 			right = mid - 1
 		} else if nums[mid] < target {
 			left = mid + 1
@@ -40,10 +73,15 @@ func searchInsert(nums []int, target int) int {
 }
 
 func main() {
-	nums := []int{1, 3, 5, 6}
+	//nums := []int{1, 3, 5, 6}
 	//aa := searchInsert(nums,5)
 	//aa := searchInsert(nums,0)
 	//aa := searchInsert(nums,2)
-	aa := searchInsert(nums, 7)
+
+	nums := []int{1, 3, 3, 3, 5, 6}
+	//aa := searchInsert(nums, 3)
+	//aa := searchInsert(nums, 7)
+	//aa := searchInsert(nums, 0)
+	aa := searchInsert(nums, 4)
 	println(aa)
 }
