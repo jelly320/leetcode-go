2b879e7f54c66688404f3bb174ef66e9419cad12
jelly320 <snsdzjlz@163.com>
2021-02-24 12:07:38 +0800

    v0:内存消耗beat 66%,看再看下怎么优化

--- Array/二分查找/0374. Guess Number Higher or Lower.go
+++ Array/二分查找/0374. Guess Number Higher or Lower.go
@@ -18,11 +18,12 @@ package main
 前提条件:
     1 <= n <= 231 - 1
     1 <= pick <= n
+	还有一个重要前提：每轮游戏，我都会从 1 到 n 随机选择一个数字。　即意味着pick这个数字的范围是在１－n之间的ｓ. 所以依旧还是个范围确定的二分查找...
 */
 
 /*模拟guess*/
 func guess(num int) int {
-	pick := 10
+	pick := 6
 	if pick < num {
 		return -1
 	} else if pick > num {
@@ -33,29 +34,25 @@ func guess(num int) int {
 	return 0
 }
 
-/*v0
-这个题的难点在于,怎么划定边界呢...
-*/
-
+/*v0*/
 func guessNumber(n int) int {
-	//把n当成mid的值
-	step := 100
-	for {
-		if guess(n) == 0 {
-			return n
-		} else if guess(n) == 1 {
-			n = n - 1
-		} else if guess(n) == -1 {
-			n = n + 1
+	left, right := 0, n-1
+	for left <= right {
+		mid := left + (right-left)/2
+		if guess(mid+1) == 0 {
+			return mid + 1
+		} else if guess(mid+1) == 1 {
+			//nums[mid]<pick,右移
+			left = mid + 1
+		} else if guess(mid+1) == -1 {
+			right = mid - 1
 		}
 	}
-
 	return -1
 }
 
 func main() {
-	//aa := guessNumber(10) //pick:=6
-	//aa := guessNumber(10) //pick:=10
-	aa := guessNumber(60) //pick:=560
+	//aa := guessNumber(6) //pick:=6
+	aa := guessNumber(60) //pick:=6
 	println(aa)
 }
