b3cd03446d9ee593a5cb9bd5ee979eb1de6afa3d
jelly320 <snsdzjlz@163.com>
2021-03-29 22:43:39 +0800

    v0 update

--- leetcode/Tree/0111. Minimum Depth of Binary Tree.go
+++ leetcode/Tree/0111. Minimum Depth of Binary Tree.go
@@ -14,13 +14,25 @@ package leetcode
 执行用时：260 ms, 在所有 Go 提交中击败了65.90%的用户
 内存消耗：21.8 MB, 在所有 Go 提交中击败了13.81%的用户
 */
+//func minDepth(root *TreeNode) int {
+//	if root == nil {
+//		return 0
+//	}
+//	if root.Right == nil && root.Left != nil {
+//		return minDepth(root.Left) + 1
+//	} else if root.Left == nil && root.Right != nil {
+//		return minDepth(root.Right) + 1
+//	}
+//	return min(minDepth(root.Right), minDepth(root.Left)) + 1
+//}
+
 func minDepth(root *TreeNode) int {
 	if root == nil {
 		return 0
 	}
-	if root.Right == nil && root.Left != nil {
+	if root.Right == nil {
 		return minDepth(root.Left) + 1
-	} else if root.Left == nil && root.Right != nil {
+	} else if root.Left == nil {
 		return minDepth(root.Right) + 1
 	}
 	return min(minDepth(root.Right), minDepth(root.Left)) + 1
