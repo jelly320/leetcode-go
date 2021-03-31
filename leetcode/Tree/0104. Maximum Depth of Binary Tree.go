1f27fa0f530aa1611ac2763bc9c409d645e095f4
jelly320 <snsdzjlz@163.com>
2021-03-31 11:12:21 +0800

    v0

--- leetcode/Tree/0104. Maximum Depth of Binary Tree.go
+++ leetcode/Tree/0104. Maximum Depth of Binary Tree.go
@@ -1,7 +1,5 @@
 package leetcode
 
-import "fmt"
-
 // https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/
 
 /**
@@ -14,43 +12,16 @@ import "fmt"
  */
 
 func maxDepth(root *TreeNode) int {
-	//leftDepth:=1
-	//rightDepth:=1
 	if root == nil {
 		return 0
 	}
-	var result []int
-	var depth int
-	operator(root, &depth, &result)
-	fmt.Println(result)
-	//operator(root, &leftDepth, &rightDepth)
-	//if leftDepth >= rightDepth {
-	//	return leftDepth
-	//} else {
-	//	return rightDepth
-	//}
-	return 1
-
+	return max104(maxDepth(root.Right), maxDepth(root.Left)) + 1
 }
 
-//func operator(root *TreeNode, left_depth *int,right_depth *int,result *[]int)  {
-//	if root != nil {
-//		*depth++
-//		operator(root.Left,left_depth,result)
-//		operator(root.Right,right_depth,result)
-//	}
-//	*result=append(*result,*depth)
-//}
-
-//func operator(root *TreeNode, leftDepth *int, rightDepth *int) {
-//	if root != nil {
-//		if root.Left != nil {
-//			*leftDepth++
-//			operator(root.Left, leftDepth, rightDepth)
-//		}
-//		if root.Right != nil {
-//			*rightDepth++
-//			operator(root.Right, leftDepth, rightDepth)
-//		}
-//	}
-//}
+func max104(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
