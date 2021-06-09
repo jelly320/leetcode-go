c6f037040e3273f18e8f898c599dc1b748ba8b04
jelly320 <snsdzjlz@163.com>
2021-06-09 23:16:23 +0800

    58

---
+++ offer/58_test.go
@@ -0,0 +1,26 @@
+package offer
+
+import "testing"
+
+func Test_reverseLeftWords(t *testing.T) {
+	type args struct {
+		s string
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+		{name: "test0", args: args{s: "abcdefg", n: 2}, want: "cdefgab"},
+		{name: "test1", args: args{s: "lrloseumgh", n: 6}, want: "umghlrlose"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverseLeftWords(tt.args.s, tt.args.n); got != tt.want {
+				t.Errorf("reverseLeftWords() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
