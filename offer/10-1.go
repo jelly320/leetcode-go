a1a7ccaae0fa25bcc230e2946282387728bcfe40
jelly320 <snsdzjlz@163.com>
2021-06-22 00:07:05 +0800

    10-1

---
+++ offer/10-1.go
@@ -0,0 +1,23 @@
+package offer
+
+/*
+写一个函数，输入 n ，求斐波那契（Fibonacci）数列的第 n 项（即 F(N)）。斐波那契数列的定义如下：
+F(0) = 0,   F(1) = 1
+F(N) = F(N - 1) + F(N - 2), 其中 N > 1.
+
+ f(n + 1) = f(n) + f(n - 1)
+*/
+
+func fib(n int) int {
+	if n == 0 || n == 1 {
+		return n
+	}
+	res := make([]int, n+1)
+	res[0] = 0
+	res[1] = 1
+	for i := 2; i <= n; i++ {
+		res[i] = (res[i-1] + res[i-2]) % 1000000007
+	}
+	return res[n]
+
+}
